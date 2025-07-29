afc9f2b5564f56020135915140d1806c33cb66f8
Luda Modna <ludmilamodna3@gmail.com>
2025-07-29 15:55:35 +0300

    Add company number to constans

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,6 +2,9 @@ const env = process.env.NODE_ENV || 'development';
 const serverIP = 'localhost';
 const serverPort = 5010;
 export default {
+  COMPATY_CONTACTS: {
+    PHONE_NUMBER: '(877)355-3585',
+  },
   CUSTOMER: 'customer',
   CREATOR: 'creator',
   CONTEST_STATUS_ACTIVE: 'active',
